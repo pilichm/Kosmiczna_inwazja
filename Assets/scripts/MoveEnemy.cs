8e7457750df557c710c55a15306cb3112dc05125
michal.pilichowski@onet.pl <michal.pilichowski@onet.pl>
2022-11-06 17:15:01 +0100

    Randomised enemy movement by assigning random movement speed, rotation and direction

--- Assets/scripts/MoveEnemy.cs
+++ Assets/scripts/MoveEnemy.cs
@@ -5,20 +5,33 @@ using UnityEngine;
 public class MoveEnemy : MonoBehaviour
 {
     private float horizontalMoveBoundary = 10.0f;
-    private float speed = 2.0f;
+    private float speed;
+    private float rotationSpeed;
     private float initialX;
+    private float moveDirection;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Randomise enemy movement - rotation, speed and direction.
         initialX = transform.position.x;
-        Debug.Log(initialX);
+        moveDirection = UnityEngine.Random.Range(-1, 1);
+        speed = UnityEngine.Random.Range(2, 6);
+        rotationSpeed = UnityEngine.Random.Range(1, 10);
+
+        if (moveDirection < 0)
+        {
+            speed *= -1;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.Translate(-1 * Vector3.right * speed * Time.deltaTime);
-        
+        transform.Translate(1 * Vector3.right * speed * Time.deltaTime);
+        transform.eulerAngles = new Vector3(
+            transform.eulerAngles.x,
+            transform.eulerAngles.y + rotationSpeed,
+            transform.eulerAngles.z);
     }
 }

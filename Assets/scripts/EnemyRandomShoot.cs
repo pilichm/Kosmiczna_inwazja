900895363966ba0c97c99d066c6a79182b0981a9
michal.pilichowski@onet.pl <michal.pilichowski@onet.pl>
2022-11-06 17:18:08 +0100

    Fixed issue making all enemies shooting at the same time

--- Assets/scripts/EnemyRandomShoot.cs
+++ Assets/scripts/EnemyRandomShoot.cs
@@ -6,17 +6,17 @@ public class EnemyRandomShoot : MonoBehaviour
 {
     public GameObject enemyLaserPrefab;
     private System.Random rnd;
-    private int spawnDelay;
-    private int spawnInterval;
+    private int shootDelay;
+    private int shootInterval;
     private int maxIntervalTime = 30;
 
     // Start is called before the first frame update
     void Start()
     {
-        rnd = new System.Random();
-        spawnDelay = rnd.Next(0, maxIntervalTime);
-        spawnInterval = rnd.Next(0, maxIntervalTime);
-        InvokeRepeating("Shoot", spawnDelay, spawnInterval);
+        // Randomise time when enemy starts shooting and delay between each shot.
+        shootDelay = UnityEngine.Random.Range(0, maxIntervalTime);
+        shootInterval = UnityEngine.Random.Range(0, maxIntervalTime);
+        InvokeRepeating("Shoot", shootDelay, shootInterval);
     }
 
     // Update is called once per frame
@@ -25,6 +25,7 @@ public class EnemyRandomShoot : MonoBehaviour
         
     }
 
+    // Make enemy shoot laser down the screen.
     void Shoot()
     {
         Instantiate(enemyLaserPrefab, transform.position, Quaternion.Euler(90, 0, 0));
